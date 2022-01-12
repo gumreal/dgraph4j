4d1a480215d39560c03c7ab7245c6139cc95bd32
gumreal <gumreal@gumreal-ThinkPad-T490>
2022-01-12 14:55:52 +0800

    set command

--- src/main/java/io/dgraph/wrapper/mutation/MutationSet.java
+++ src/main/java/io/dgraph/wrapper/mutation/MutationSet.java
@@ -1,13 +1,71 @@
 package io.dgraph.wrapper.mutation;
 
+import com.google.gson.Gson;
 import com.google.protobuf.ByteString;
 import io.dgraph.DgraphClient;
 import io.dgraph.DgraphProto;
 import io.dgraph.Transaction;
+import io.dgraph.wrapper.schema.DgraphTypeBase;
+
+import java.util.List;
 
 /** */
 public class MutationSet {
+
   /**
+   * send a Dgraph Type object to Set Command
+   *
+   * @param client
+   * @param obj
+   */
+  public static void set(DgraphClient client, DgraphTypeBase obj){
+    if(null == obj){
+      return;
+    }
+    Transaction txn = client.newTransaction();
+    try {
+      DgraphProto.Mutation mutation = DgraphProto.Mutation.newBuilder()
+              .setSetJson(ByteString.copyFromUtf8(obj.toJson()))
+              .build();
+      DgraphProto.Response res = txn.mutate(mutation);
+      System.out.println(res.toString());
+
+      txn.commit();
+
+    } finally {
+      txn.discard();
+    }
+  }
+
+  /**
+   * send list of Dgraph Type objects to Set Command
+   *
+   * @param client
+   * @param list
+   */
+  public static void set(DgraphClient client, List<DgraphTypeBase> list){
+    if(null == list || list.size()==0){
+      return;
+    }
+
+    Transaction txn = client.newTransaction();
+    try {
+      DgraphProto.Mutation mutation = DgraphProto.Mutation.newBuilder()
+              .setSetJson(ByteString.copyFromUtf8(new Gson().toJson(list)))
+              .build();
+      DgraphProto.Response res = txn.mutate(mutation);
+      System.out.println(res.toString());
+
+      txn.commit();
+
+    } finally {
+      txn.discard();
+    }
+  }
+
+  /**
+   * send raw json for Set Command
+   *
    * @param client
    * @param json
    */
@@ -16,7 +74,9 @@ public class MutationSet {
     try {
       DgraphProto.Mutation mutation =
           DgraphProto.Mutation.newBuilder().setSetJson(ByteString.copyFromUtf8(json)).build();
-      txn.mutate(mutation);
+      DgraphProto.Response res = txn.mutate(mutation);
+      System.out.println(res.toString());
+
       txn.commit();
     } finally {
       txn.discard();
---
+++ src/main/java/io/dgraph/wrapper/schema/SchemaHelper.java
@@ -0,0 +1,120 @@
+package io.dgraph.wrapper.schema;
+
+import io.dgraph.DgraphClient;
+import io.dgraph.DgraphProto;
+import io.dgraph.wrapper.dic.DataType;
+import io.dgraph.wrapper.dic.DateTimeIndexType;
+import io.dgraph.wrapper.dic.StringIndexType;
+
+import java.util.Set;
+
+/** Schema Tool */
+public class SchemaHelper {
+
+  /**
+   * alter predicate
+   *
+   * @param client
+   * @param predicate
+   * @param dt
+   */
+  public static void alterPredicate(DgraphClient client, String predicate, DataType dt) {
+    alterPredicate(client, predicate, dt, false);
+  }
+
+  /**
+   * alter predicate
+   *
+   * @param client
+   * @param predicate
+   * @param dt
+   */
+  public static void alterPredicate(
+      DgraphClient client, String predicate, DataType dt, boolean createIndex) {
+    String indexName = "";
+    if (createIndex) {
+      switch (dt) {
+        case DT_STRING:
+          indexName = StringIndexType.exact.name();
+          break;
+        case DT_DATETIME:
+          indexName = DateTimeIndexType.day.name();
+        default:
+          indexName = dt.toString();
+      }
+      alterPredicate(client, predicate, dt, true, indexName);
+    } else {
+      alterPredicate(client, predicate, dt, false, indexName);
+    }
+  }
+
+  /**
+   * alter predicate
+   *
+   * @param client
+   * @param predicate
+   * @param sit
+   */
+  public static void alterPredicate(DgraphClient client, String predicate, StringIndexType sit) {
+    alterPredicate(client, predicate, DataType.DT_STRING, true, sit.name());
+  }
+
+  /**
+   * alter predicate
+   *
+   * @param client
+   * @param predicate
+   * @param dtit
+   */
+  public static void alterPredicate(DgraphClient client, String predicate, DateTimeIndexType dtit) {
+    alterPredicate(client, predicate, DataType.DT_DATETIME, true, dtit.name());
+  }
+
+  /**
+   * alter predicate
+   *
+   * @param predicate
+   * @param dt
+   * @param createIndex
+   * @param indexName
+   * @return
+   */
+  private static void alterPredicate(
+      DgraphClient client, String predicate, DataType dt, boolean createIndex, String indexName) {
+    String schema = String.format("%s: %s", predicate, dt.toString());
+    if (createIndex) {
+      schema += String.format(" @index(%s)", indexName);
+    }
+    DgraphProto.Operation op = DgraphProto.Operation.newBuilder().setSchema(schema).build();
+    client.alter(op);
+  }
+
+  /**
+   * alter dgraph.type
+   *
+   * @param client
+   * @param typeName
+   * @param predicates
+   */
+  public static void alterType(DgraphClient client, String typeName, Set<String> predicates){
+    StringBuffer buffer = new StringBuffer();
+    buffer.append(typeName + " {\n");
+    if(null != predicates){
+      predicates.forEach(s -> buffer.append("   " + s + "\n"));
+    }
+    buffer.append("}");
+    DgraphProto.Operation op = DgraphProto.Operation.newBuilder().setSchema(buffer.toString()).build();
+    client.alter(op);
+  }
+
+  /**
+   * set raw dgraph schema
+   *
+   * @param client
+   * @param schema
+   */
+  public static void alter(DgraphClient client, String schema){
+    DgraphProto.Operation op = DgraphProto.Operation.newBuilder().setSchema(schema).build();
+    client.alter(op);
+  }
+}
